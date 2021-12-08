dddf9864a5cef0da76b320eab97076b5b11f7104
Madrog <rogers.madda@gmail.com>
2021-12-09 02:55:59 +0300

    Added Followers, Logging & Unit Tests

--- microblog/config.py
+++ microblog/config.py
@@ -10,3 +10,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     ADMINS = ['your-email@example.com']
+    POSTS_PER_PAGE=3
+
+    
