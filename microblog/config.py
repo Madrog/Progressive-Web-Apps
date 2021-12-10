151d902bf30f36da123566dea1089f4a08763b99
Madrog <rogers.madda@gmail.com>
2021-12-10 08:58:09 +0300

    Added Pagination

--- microblog/config.py
+++ microblog/config.py
@@ -10,6 +10,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     ADMINS = ['your-email@example.com']
-    POSTS_PER_PAGE=3
+    POSTS_PER_PAGE=25
 
-    
+
+
