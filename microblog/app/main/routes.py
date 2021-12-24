c4de7bf6bc744a0fff88fe6af2c8d0bcc5e59704
Madrog <rogers.madda@gmail.com>
2021-12-24 22:29:14 +0300

    Fixed Bugs

--- microblog/app/main/routes.py
+++ microblog/app/main/routes.py
@@ -1,13 +1,15 @@
 from datetime import datetime
-from flask import current_app, flash, url_for, redirect, g, request, render_template, jsonify
+from flask import render_template, flash, redirect, url_for, request, g, \
+    jsonify, current_app
 from flask_login import current_user, login_required
 from flask_babel import _, get_locale
 from langdetect import detect, LangDetectException
 from app import db
-from app.main import bp
-from app.main.forms import EditProfileForm, EmptyForm, PostForm, SearchForm, MessageForm
-from app.models import User, Post
+from app.main.forms import EditProfileForm, EmptyForm, PostForm, SearchForm, \
+    MessageForm
+from app.models import User, Post, Message, Notification
 from app.translate import translate
+from app.main import bp
 
 
 @bp.before_app_request
@@ -16,8 +18,7 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
         g.search_form = SearchForm()
-    g.locale = str(get_locale)
-
+    g.locale = str(get_locale())
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -30,7 +31,8 @@ def index():
             language = detect(form.post.data)
         except LangDetectException:
             language = ''
-        post = Post(body=form.post.data, author=current_user, language=language)
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
@@ -40,42 +42,42 @@ def index():
         page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('main.index', page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('main.index', page=posts.next_num) \
+    prev_url = url_for('main.index', page=posts.prev_num) \
         if posts.has_prev else None
-    return render_template('index.html', title=_('Home'), form=form, 
-                            posts=posts.items, next_url=next_url, prev_url=prev_url)
+    return render_template('index.html', title=_('Home'), form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
 @bp.route('/explore')
 @login_required
 def explore():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.timestap.desc()).paginate(
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('main.explore', page=posts.next_num) \
         if posts.has_next else None
     prev_url = url_for('main.explore', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('index.html', title=_('Explore'),
-                            posts=posts.items, next_url=next_url,
-                            prev_url=prev_url)
-
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
 @bp.route('/user/<username>')
 @login_required
 def user(username):
-    user = User.query.filter_by(usename=username).first_or_404()
+    user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.user', username=username, 
-                        page=posts.next_url) if posts.has_next else None
-    prev_url = url_for('main.user', username=username, 
-                        page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for('main.user', username=user.username,
+                       page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.user', username=user.username,
+                       page=posts.prev_num) if posts.has_prev else None
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts.items,
-                        next_url=next_url, prev_url=prev_url, form=form)
+                           next_url=next_url, prev_url=prev_url, form=form)
 
 
 @bp.route('/user/<username>/popup')
@@ -94,15 +96,16 @@ def edit_profile():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
-        flash(_('Your change have been saved.'))
+        flash(_('Your changes have been saved.'))
         return redirect(url_for('main.edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title=_('Edit Profile'), form=form)
+    return render_template('edit_profile.html', title=_('Edit Profile'),
+                           form=form)
 
 
-@bp.route('/follow/<usernme>', methods=['POST'])
+@bp.route('/follow/<username>', methods=['POST'])
 @login_required
 def follow(username):
     form = EmptyForm()
@@ -118,6 +121,8 @@ def follow(username):
         db.session.commit()
         flash(_('You are following %(username)s!', username=username))
         return redirect(url_for('main.user', username=username))
+    else:
+        return redirect(url_for('main.index'))
 
 
 @bp.route('/unfollow/<username>', methods=['POST'])
@@ -130,7 +135,7 @@ def unfollow(username):
             flash(_('User %(username)s not found.', username=username))
             return redirect(url_for('main.index'))
         if user == current_user:
-            flash(_('You cannt unfollow yourself!'))
+            flash(_('You cannot unfollow yourself!'))
             return redirect(url_for('main.user', username=username))
         current_user.unfollow(user)
         db.session.commit()
@@ -140,13 +145,12 @@ def unfollow(username):
         return redirect(url_for('main.index'))
 
 
-
 @bp.route('/translate', methods=['POST'])
 @login_required
 def translate_text():
-    return jsonify({'text': translate(request.form['text'], 
-                                    request.form['source_language'],
-                                    request.form['dest_language'])})
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
 
 
 @bp.route('/search')
@@ -155,15 +159,14 @@ def search():
     if not g.search_form.validate():
         return redirect(url_for('main.explore'))
     page = request.args.get('page', 1, type=int)
-    posts, total = Post.search(g.search_form.q.data, page, current_app.config['POSTS_PER_PAGE'])
-
+    posts, total = Post.search(g.search_form.q.data, page,
+                               current_app.config['POSTS_PER_PAGE'])
     next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
         if total > page * current_app.config['POSTS_PER_PAGE'] else None
-    prev_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
         if page > 1 else None
-    return render_template('search.html', title=_('Search'), posts=posts, 
-                            next_url=next_url, prev_url=prev_url)
-
+    return render_template('search.html', title=_('Search'), posts=posts,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
@@ -173,14 +176,14 @@ def send_message(recipient):
     form = MessageForm()
     if form.validate_on_submit():
         msg = Message(author=current_user, recipient=user,
-                        body=form.message.data)
+                      body=form.message.data)
         db.session.add(msg)
         user.add_notification('unread_message_count', user.new_messages())
         db.session.commit()
         flash(_('Your message has been sent.'))
         return redirect(url_for('main.user', username=recipient))
-    return render_template('send_message.html', title=_('Send Message'), 
-                            form=form, recipient=recipient)
+    return render_template('send_message.html', title=_('Send Message'),
+                           form=form, recipient=recipient)
 
 
 @bp.route('/messages')
@@ -191,13 +194,14 @@ def messages():
     db.session.commit()
     page = request.args.get('page', 1, type=int)
     messages = current_user.messages_received.order_by(
-        Message.timestap.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
+        Message.timestamp.desc()).paginate(
+            page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('main.messages', page=messages.next_num) \
         if messages.has_next else None
     prev_url = url_for('main.messages', page=messages.prev_num) \
         if messages.has_prev else None
-    return render_template('message.html', messages=messages.items, 
-                            next_url=next_url, prev_url=prev_url)
+    return render_template('messages.html', messages=messages.items,
+                           next_url=next_url, prev_url=prev_url)
 
 
 @bp.route('/export_posts')
@@ -206,7 +210,7 @@ def export_posts():
     if current_user.get_task_in_progress('export_posts'):
         flash(_('An export task is currently in progress'))
     else:
-        current_user.launch_task('export_posts')
+        current_user.launch_task('export_posts', _('Exporting posts...'))
         db.session.commit()
     return redirect(url_for('main.user', username=current_user.username))
 
@@ -217,11 +221,8 @@ def notifications():
     since = request.args.get('since', 0.0, type=float)
     notifications = current_user.notifications.filter(
         Notification.timestamp > since).order_by(Notification.timestamp.asc())
-    return jsonify([{ 
-        'name' : n.name, 
-        'data': n.get_data(), 
-        'timestamp': n.timestamp 
-        } for n in notifications])
-
-
-
+    return jsonify([{
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications])
--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,15 +1,15 @@
-import os
 import base64
-import json
-import jwt
-import rq
-import redis
 from datetime import datetime, timedelta
-from time import time
 from hashlib import md5
+import json
+import os
+from time import time
 from flask import current_app, url_for
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import redis
+import rq
 from app import db, login
 from app.search import add_to_index, remove_from_index, query_index
 
@@ -57,13 +57,38 @@ db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
 db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
 
 
-followers = db.Table('followers', 
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page, per_page, False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page,
+                                **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
+
+
+followers = db.Table(
+    'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
 
-class User(UserMixin, db.Model):
+class User(UserMixin, PaginatedAPIMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -71,11 +96,23 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
     followed = db.relationship(
-        'User', secondary=followers, 
+        'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    messages_sent = db.relationship('Message',
+                                    foreign_keys='Message.sender_id',
+                                    backref='author', lazy='dynamic')
+    messages_received = db.relationship('Message',
+                                        foreign_keys='Message.recipient_id',
+                                        backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user',
+                                    lazy='dynamic')
+    tasks = db.relationship('Task', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -88,12 +125,13 @@ class User(UserMixin, db.Model):
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
-    
+
     def unfollow(self, user):
         if self.is_following(user):
             self.followed.remove(user)
@@ -101,7 +139,7 @@ class User(UserMixin, db.Model):
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
-    
+
     def followed_posts(self):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
@@ -109,46 +147,46 @@ class User(UserMixin, db.Model):
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
-
     def get_reset_password_token(self, expires_in=600):
-        return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in}, 
-                            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
-    
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'], algorithm='HS256')
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            uid = jwt.decode(token, current_app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
-        except Exception as e:
-            print(e)
+        except:
             return
-        return User.query.get(uid)
-
+        return User.query.get(id)
 
     def new_messages(self):
         last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
-        return Message.query.filter_by(recipient=self).filter(Message.timestamp > last_read_time).count()
-    
-    
+        return Message.query.filter_by(recipient=self).filter(
+            Message.timestamp > last_read_time).count()
+
     def add_notification(self, name, data):
         self.notifications.filter_by(name=name).delete()
         n = Notification(name=name, payload_json=json.dumps(data), user=self)
         db.session.add(n)
         return n
 
-
     def launch_task(self, name, description, *args, **kwargs):
-        rq_job = current_app.task_queue.enqueu('app.tasks.' + name, self.id, *args, **kwargs)
-        task = Task(id=rq_job.get_id(), name=name, description=description, user=self)
+        rq_job = current_app.task_queue.enqueue('app.tasks.' + name, self.id,
+                                                *args, **kwargs)
+        task = Task(id=rq_job.get_id(), name=name, description=description,
+                    user=self)
         db.session.add(task)
         return task
 
     def get_tasks_in_progress(self):
         return Task.query.filter_by(user=self, complete=False).all()
-    
+
     def get_task_in_progress(self, name):
-        return Task.query.filter_by(name=name, user=self, complete=False).first()
-    
+        return Task.query.filter_by(name=name, user=self,
+                                    complete=False).first()
+
     def to_dict(self, include_email=False):
         data = {
             'id': self.id,
@@ -160,12 +198,11 @@ class User(UserMixin, db.Model):
             'followed_count': self.followed.count(),
             '_links': {
                 'self': url_for('api.get_user', id=self.id),
-                'followers' : url_for('api.get_followers', id=self.id),
+                'followers': url_for('api.get_followers', id=self.id),
                 'followed': url_for('api.get_followed', id=self.id),
                 'avatar': self.avatar(128)
             }
         }
-
         if include_email:
             data['email'] = self.email
         return data
@@ -173,7 +210,7 @@ class User(UserMixin, db.Model):
     def from_dict(self, data, new_user=False):
         for field in ['username', 'email', 'about_me']:
             if field in data:
-                setattr(self, field, data['field'])
+                setattr(self, field, data[field])
         if new_user and 'password' in data:
             self.set_password(data['password'])
 
@@ -186,19 +223,15 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return self.token
 
-    def revote_token(self):
-        self.token_expiration = datetime.ntcnow() - timedelta(seconds=1)
-
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
 
     @staticmethod
     def check_token(token):
         user = User.query.filter_by(token=token).first()
         if user is None or user.token_expiration < datetime.utcnow():
             return None
-        return user 
-        
-
-
+        return user
 
 
 @login.user_loader
@@ -215,7 +248,7 @@ class Post(SearchableMixin, db.Model):
     language = db.Column(db.String(5))
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)     
+        return '<Post {}>'.format(self.body)
 
 
 class Message(db.Model):
@@ -233,7 +266,7 @@ class Notification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    timestamp = db.Column(db.Float, index=True, defalut=time)
+    timestamp = db.Column(db.Float, index=True, default=time)
     payload_json = db.Column(db.Text)
 
     def get_data(self):
@@ -257,4 +290,3 @@ class Task(db.Model):
     def get_progress(self):
         job = self.get_rq_job()
         return job.meta.get('progress', 0) if job is not None else 100
-
