5ff3e84f36e2e1f4d8f3e38a6188184cc36ec8a8
Madrog <rogers.madda@gmail.com>
2021-12-31 23:32:42 +0300

    Added Flask CLI tools

--- blog/app/app.py
+++ blog/app/app.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask.cli import FlaskGroup
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 from config import Configuration # import our configuration data
 
 app = Flask(__name__)
 app.config.from_object(Configuration) # use value from our configuration object
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+cli = FlaskGroup(app)
+
+
+
---
+++ blog/app/manage.py
@@ -0,0 +1,6 @@
+from app import manager
+from main import *
+
+if __name__ == '__main__':
+    cli()
+    manager.run()
