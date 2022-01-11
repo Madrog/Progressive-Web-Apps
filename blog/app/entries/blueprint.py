160407fb173f5cb1d9867f498488a8a01c525840
Madrog <rogers.madda@gmail.com>
2022-01-11 23:33:41 +0300

    Added deletion pages

--- blog/app/entries/blueprint.py
+++ blog/app/entries/blueprint.py
@@ -68,3 +68,14 @@ def edit(slug):
     else:
         form = EntryForm(obj=entry)
     return render_template('entries/edit.html', entry=entry, form=form)
+
+
+@entries.route('/<slug>/delete/', methods=['GET', 'POST'])
+def delete(slug):
+    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    if request.method == 'POST':
+        entry.status = Entry.STATUS_DELETED
+        db.session.add(entry)
+        db.session.commit()
+        return redirect(url_for('entries.index'))
+    return render_template('entries/delete.html', entry=entry)
