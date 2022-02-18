3c578ab5d53561cca6f928442f92f45cb7e381ca
Madrog <rogers.madda@gmail.com>
2022-02-19 00:12:35 +0300

    added comments.js corrections

--- blog/app/static/js/comments.js
+++ blog/app/static/js/comments.js
@@ -20,6 +20,38 @@ Comments = window.Comments || {};
         function getFormData(form) {
             return {
                 'name': form.find('input$name').val(),
-            };
+                'email': form.find('input#email').val(),
+                'url' : form.find('input#url').val(),
+                'body': form.find('textarea#body').val(),
+                'entry_id': form.find('input[name=entry_id]').val()
+            }
         }
-})
+
+        function bindHandler() {
+            /* When the comment form is submitted, serialize the form data as JSON 
+                and POST it to the API. */
+            $('form#comment-form').on('submit', function () {
+                var form = $(this);
+                var formData = getFormData(form);
+                var request = $.ajax({
+                    url: form.attr('action'),
+                    type: 'POST',
+                    data: JSON.stringify(formData),
+                    contentType: 'application/json; charset=utf-8',
+                    dataType: 'json'
+                });
+                request.success(function(data) {
+                    alertDiv = maketAlert('success', 'Success', 'your comment was posted.');
+                    form.before(alertDiv);
+                    form[0].reset();               
+                });
+                request.fail(function() {
+                    alertDiv = makeAlert('danger', 'Error', 'your comment was not posted.');
+                    form.before(alertDiv);
+                });
+                return false;
+            });
+        }
+
+    exports.bindHandler = bindHandler;
+}) (Comments, jQuery)
